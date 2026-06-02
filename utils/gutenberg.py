803595a6200fbc5abd9179247722a26483e04888
Jeremie Nagi <jeremie.nagi@epitech.eu>
2026-06-02 13:41:38 +0200

    Feat : Cut book header + footer

--- utils/gutenberg.py
+++ utils/gutenberg.py
@@ -1,6 +1,8 @@
 import requests
 from pathlib import Path
+from text_processing import remove_header
 
+# Download full book.txt
 def get_book(book_id):
     url = f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt"
     
@@ -9,15 +11,18 @@ def get_book(book_id):
     
     return response.text
 
+# Save book in data/books
 def save_book(book_id, text):
     path = Path("data/books") / f"{book_id}.txt"
     with open (path, "w", encoding="utf8") as file:
         file.write(text)
-    
+
+# Main logic
 def main ():
     book_id = input("Enter book id : ")
-    text = get_book(book_id)
-    save_book(book_id, text)
+    full_text = get_book(book_id)
+    cut_text = remove_header(full_text)
+    save_book(book_id, cut_text)
     print (f"Book {book_id} saved.") 
     
 if __name__ == "__main__":
---
+++ utils/text_processing.py
@@ -0,0 +1,22 @@
+def remove_header(text):
+    header = "*** START OF THE PROJECT GUTENBERG EBOOK"
+    footer = "*** END OF THE PROJECT GUTENBERG EBOOK"
+    
+    start_index = text.find(header)
+    end_index = text.find(footer)
+    
+    # No header
+    if start_index == -1:
+        return text
+
+    # Header start after \n
+    cut_header = text.find("\n", start_index)
+    if cut_header == -1:
+        return text
+    cut_header += 1
+        
+    # Len text without Footer
+    if end_index == -1:
+       end_index = len(text)
+       
+    return text[cut_header:end_index].strip()
