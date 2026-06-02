5d8716967ad03a7b135b0d71a0f0ec973e4e640a
Jeremie Nagi <jeremie.nagi@epitech.eu>
2026-06-02 10:43:22 +0200

    Feat : Download and save book.txt

--- utils/gutenberg.py
+++ utils/gutenberg.py
@@ -9,19 +9,16 @@ def get_book(book_id):
     
     return response.text
 
-
 def save_book(book_id, text):
     path = Path("data/books") / f"{book_id}.txt"
     with open (path, "w", encoding="utf8") as file:
         file.write(text)
-        
-        
+    
 def main ():
     book_id = input("Enter book id : ")
     text = get_book(book_id)
     save_book(book_id, text)
-    print (f"Book {book_id} saved.")
+    print (f"Book {book_id} saved.") 
     
-
 if __name__ == "__main__":
     main()
